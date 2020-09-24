07ee67626680d794b01ccf28ae32986b244da071
Ricky Rodriguez <ricardo.rodriguez4@yahoo.com>
2020-09-24 10:38:24 -0500

    starting arrays

--- practice.js
+++ practice.js
@@ -42,8 +42,70 @@ class SLL{
             currentNode = currentNode.next;
         }
     }
+    }
+
+//view(my_node);
+
+console.log('--------------------------------')
+//To Do 1
+//Push Front
+//Given an array and an additional value, insert this value at the beginning of the array. 
+//Do this without using any built-in array methods.
+console.log('Push Front');
+function pushFront(arr, val){
+    console.log(arr + ', New value to push: ' + val);
+    for(var i = arr.length; i>0; i--){
+        arr[i] = arr[i-1];
+
+    }
+    arr[0] = val;
+    console.log(arr)
 }
 
-view(my_node);
+pushFront([0,2,4,6,8,10], 5)
+console.log('----------------------------------');
+
+
+console.log('Pop Front')
+//Pop Front
+//Given an array, remove and return the value at the beginning of the array. 
+//Do this without using any built-in array methods except pop().
+function popFront(arr){
+    console.log(arr)
+    var first_value = arr[0];
+    var temp;
+    for(var i = 0; i < arr.length-1;i++){
+        temp = arr[i];
+        arr[i]=arr[i+1];
+        arr[i+1]=temp;
+    }
+    arr.pop();
+    console.log('New Array:', arr);
+    return first_value;
+}
+
+console.log(popFront([1,2,3,4,5]))
+
+
+console.log('----------------------------------');
+
+
+console.log('Insert At')
+//Insert At
+//Given an array, index, and additional value, insert the value into array at given index. 
+//Do this without using built-in array methods. 
+//You can think of pushFront(arr,val) as equivalent to insertAt(arr,0,val).
+
+function insertAt(arr, index, val){
+    console.log(arr, '- Insert:', val, '@ index:',index);
+    
+
+}
+
+insertAt(['hello', 'world', 'my', 'name', 'is', 'Ricky'], 4, 'Tomorrow')
+
+
+
+
 
 
