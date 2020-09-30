7e3d08e7a59b3863a39b3c791d134f7d8a8bd067
Ricky Rodriguez <ricardo.rodriguez4@yahoo.com>
2020-09-30 00:10:15 -0500

    loop practice

--- pythonBasics.py
+++ pythonBasics.py
@@ -1,4 +1,14 @@
-a = int(input('Enter number a:'))
-b = int(input('Enter number b:'))
-print('The sum of a and b is:', (a+b))
+#a = int(input('Enter number a:'))
+#b = int(input('Enter number b:'))
+#print('The sum of a and b is:', (a+b))
 
+array = [0,2,4,6,8]
+
+print(array)
+#print(array[2])
+
+#for item in array:
+ #   print(item)
+
+for i in range(100,10,-10):
+    print(i)
