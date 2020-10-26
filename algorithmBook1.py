b4dde668220cf8815fd02f1ea44ac54a8525a1f4
Ricky Rodriguez <ricardo.rodriguez4@yahoo.com>
2020-10-26 10:45:40 -0500

    bubble sort

--- algorithmBook1.py
+++ algorithmBook1.py
@@ -1,1 +1,13 @@
 #Bubble sort
+
+arr = [1,5,3,2,0,8]
+
+def bubbleSort(arr):
+    for i in range(len(arr)):
+        print('index', i, 'value', arr[i])
+        print('comparing', arr[i], arr[i+1])
+        if arr[i] > arr[i+1]:
+            arr[i], arr[i+1] = arr[i+1], arr[i]
+
+
+bubbleSort(arr)
---
+++ pathrise.py
@@ -0,0 +1,47 @@
+
+
+
+print("Hello")
+
+'''
+
+arr = [10, 5, -2]
+results = [10, 5]
+
+
+arr = [10, 5, 2, -7, 15]
+                  i
+
+stack = [ 10]
+result = [10, 15]
+
+
+
+
+
+
+'''
+
+def collisions(arr):
+  result = []
+  #iterate array
+  
+  for i in range(0,len(arr)):
+    j = i+1
+    while arr[i] :
+  #compare values
+    
+      
+  #if absolute value of the left is bigger than the right (pop)
+  #return the array
+  return []
+
+# Your last C/C++ code is saved below:
+# #include <iostream>
+# using namespace std;
+
+# int main() {
+# 	cout<<"Hello";
+# 	return 0;
+# }
+
