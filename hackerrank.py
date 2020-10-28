0c7c347c6520be0ccccb2475f039585c949dcf3c
Ricky Rodriguez <ricardo.rodriguez4@yahoo.com>
2020-10-27 21:13:05 -0500

    Jumping on Clouds

--- hackerrank.py
+++ hackerrank.py
@@ -14,9 +14,25 @@ def sockMerchant(n, arr):
         i += 1
 
     return pairs
-
-
-
-
-
 sockMerchant(9,[10,20,20,10,10,30,50,10,20])
+
+def jumpingOnClouds(c):
+    i = 0
+    jumps = 0 
+    while i < len(c):
+        if i == len(c)-1:
+            break
+        elif i+1 == len(c)-1 or i+2 == len(c)-1:
+            jumps += 1
+            break
+        if c[i+1] == 1:
+            jumps += 1
+            i += 2
+        elif c[i+1] == 0:
+            jumps += 1
+            if c[i+2] == 0:
+                i+=2
+            else:
+                i+=1
+    return jumps
+jumpingOnClouds([0,0,1,0,0,1,0])
