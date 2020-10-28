c5f3bb19b42a476a4dda22b986dc3fd1f51c3333
Ricky Rodriguez <ricardo.rodriguez4@yahoo.com>
2020-10-27 19:18:00 -0500

    algorithms

--- algorithmBook1.py
+++ algorithmBook1.py
@@ -1,6 +1,6 @@
 #Bubble sort
 
-arr = [7,1,6,5,3,2,4,9,0,8]
+arr = [70,1,61,55,32,22,24,29,20,28]
 
 def bubbleSort(arr):
     for j in range(len(arr)-1):
@@ -10,26 +10,25 @@ def bubbleSort(arr):
 
     return arr
 #print(bubbleSort(arr))
-
+#-----------------------------------------------------------------------------------------
 #selection Sort
-
 def selectionSort(arr):
     print(arr)
     for j in range(len(arr)-1):
         min = arr[j]
-        print('----------------', 'iteration', j)
-        print('min = ', min)
         for i in range(j+1,len(arr)):
-            print('checking', arr[i], 'at index:', i)
             if arr[i] < min:
                 min = arr[i]
                 min_index = i
-            print('new Minimum:', min)
         if arr[j] != min:
             arr[j], arr[min_index] = arr[min_index], arr[j]
-            print('swapped', arr[j], 'and', arr[min_index])
-            print(arr)
-        else:
-            print('Next value in the correct spot')
     print('Final Array:', arr)
-selectionSort(arr)
+#selectionSort(arr)
+
+#--------------------------------------------------------------------------------------------------------------------
+
+#insertion sort
+# iterate a for loop
+# iterate a reverse for loop
+def insertionSort(arr):
+    pass
